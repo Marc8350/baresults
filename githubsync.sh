c28a4df84c43870ce26a1eb94e4e92b38a45f319
Marc8350 <marc.rodig@icloud.com>
2025-03-09 16:14:44 +0100

    Update Excel files Sun 09 Mar 2025 04:14:44 PM CET

--- githubsync.sh
+++ githubsync.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-cd "/home/marc/Development/Bachelorarbeit/colliee_container/results"
+EXCEL_FILES_DIR="/home/marc/Development/Bachelorarbeit/colliee_container/results" # Replace with your actual path
+GIT_REPO_DIR="$(pwd)" # Current directory, where the git repo is.
+
+# Copy the new excel files into the git repository.
+cp "$EXCEL_FILES_DIR"/* "$GIT_REPO_DIR"
+
+cd "$GIT_REPO_DIR"
 
 git add .
 if git diff-index --quiet HEAD --; then
   echo "No changes to commit"
 else
   git commit -m "Update Excel files $(date)"
-  git push origin main
+  git push origin master
 fi
